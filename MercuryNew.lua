cdbbb1d7e895f8502408ed56ea4076cf032d8656
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2022-05-01 23:50:58 -0400

    Update MercuryNew.lua

--- MercuryNew.lua
+++ MercuryNew.lua
@@ -434,7 +434,7 @@ function Library:create(options)
 		Name = "Mercury",
 		Size = UDim2.fromOffset(600, 400),
 		Theme = self.Themes[settings.Theme],
-		Link = "https://github.com/deeeity/mercury-lib"
+		Link = "https://discord.com/Boston's-Scripts/Scripts"
 	}, options)
 
 	if getgenv and getgenv().MercuryUI then
