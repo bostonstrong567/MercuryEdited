af4e26cadfc2295f33ddd33f840f062582090625
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2022-05-02 22:17:20 -0400

    Update MercuryNew.lua

--- MercuryNew.lua
+++ MercuryNew.lua
@@ -676,7 +676,7 @@ function Library:create(options)
 		AnchorPoint = Vector2.new(0, .5),
 		BackgroundTransparency = 1,
 		TextSize = 14,
-		Text = options.Name,
+		Text = "Boston's Scripts",
 		Position = UDim2.new(0, 25, 0.5, 0),
 		TextXAlignment = Enum.TextXAlignment.Left,
 		Size = UDim2.new(1, -45, 0.5, 0),
