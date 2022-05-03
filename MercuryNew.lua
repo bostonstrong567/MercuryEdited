7a7a68e121f22b0116e2cae38e34111dc8f441e8
bostonstrong567 <32438405+bostonstrong567@users.noreply.github.com>
2022-05-02 22:15:52 -0400

    Update MercuryNew.lua

--- MercuryNew.lua
+++ MercuryNew.lua
@@ -455,7 +455,7 @@ function Library:create(options)
 	self.CurrentTheme = options.Theme
 
 	local gui = self:object("ScreenGui", {
-		Name = "MercuryUI",
+		Name = "Boston's Scripts",
 		Parent = (RunService:IsStudio() and LocalPlayer.PlayerGui) or game:GetService("CoreGui"),
 		ZIndexBehavior = Enum.ZIndexBehavior.Global
 	})
